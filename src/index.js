d9459f5a6150f0c843ed932a621f331be223d77f
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-12-01 10:13:06 +0700

    Node js starter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hallo word')
+})
+
+app.listen(port, () =>
+  console.log(`Server run on port : http://localhost:${port}'`)
+)
