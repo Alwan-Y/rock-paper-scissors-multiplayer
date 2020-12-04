d31826474eefb89b0174fbdcee846fb2928734c1
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-12-04 09:39:39 +0700

    add login

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import { User } from '../models'
-import bcrypt from 'bcrypt'
-const salt = bcrypt.genSaltSync(10)
-
-class userController {
-  static getRegister = (req, res) => {
-    res.render('createUser')
-  }
-
-  static postRegister = async (req, res) => {
-    const { body } = req
-
-    try {
-      const user = await User.register(body)
-
-      res.status(200).send(user)
-    } catch (err) {
-      res.status(400).send(err)
-    }
-  }
-
-  static LoginCheck = async (req, res) => {
-    const { username, password } = req.body
-
-    const findUser = await User.findOne({ where: {username} })
-
-
-    if (!findUser) {
-      return res.status(404).json({message: 'Account Not Found'})
-    }
-
-    const checkPassword = bcrypt.compareSync(password, findUser.password)
-
-    if (!checkPassword) {
-      return res.status(403).json({message: 'Password invalid'})
-    }
-
-    return res.status(201).json({message: 'Successfully Login'})
-  }
-}
-
-export default userController
-=======
 import { User, UserBio } from '../models'
 
 class userController {
@@ -89,22 +45,20 @@ class userController {
   static LoginCheck = async (req, res) => {
     const { username, password } = req.body
 
-    const findUser = await User.findOne({ where: {username} })
-
+    const findUser = await User.findOne({ where: { username } })
 
     if (!findUser) {
-      return res.status(404).json({message: 'Account Not Found'})
+      return res.status(404).json({ message: 'Account Not Found' })
     }
 
     const checkPassword = bcrypt.compareSync(password, findUser.password)
 
     if (!checkPassword) {
-      return res.status(403).json({message: 'Password invalid'})
+      return res.status(403).json({ message: 'Password invalid' })
     }
 
-    return res.status(201).json({message: 'Successfully Login'})
+    return res.status(201).json({ message: 'Successfully Login' })
   }
 }
 
 export default userController
->>>>>>> 5232273... add bio to register user
--- src/models/User.js
+++ src/models/User.js
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-const { Model } = require('sequelize')
-const bcrypt = require('bcrypt')
-
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-
-    static encrypt = (password) => bcrypt.hashSync(password, 8)
-
-    static register = ({ username, password, confirmPassword }) => {
-      if (password !== confirmPassword) {
-        throw new Error('Password and Confirm Password not match')
-      }
-
-      const encryptedPassword = this.encrypt(password)
-
-      console.log(encryptedPassword)
-
-      return this.create({
-        username,
-        password: encryptedPassword,
-      })
-    }
-  }
-  User.init(
-    {
-      id: {
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        unique: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      timestamps: true,
-      sequelize,
-      modelName: 'User',
-      tableName: 'User',
-    }
-  )
-  return User
-}
-=======
 'use strict'
 const { Model } = require('sequelize')
 
@@ -82,7 +21,6 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static encrypt = (password) => bcrypt.hashSync(password, 8)
-
   }
   User.init(
     {
@@ -111,4 +49,3 @@ module.exports = (sequelize, DataTypes) => {
   )
   return User
 }
->>>>>>> 5232273... add bio to register user
