73f0282fdc21ed2f0bb22651bd5a389a3af6aedb
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-12-05 15:33:39 +0700

    add logout

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import userController from '../controller/user'
+import { auth } from '../middleware'
 
 const router = express.Router()
 
@@ -8,5 +9,6 @@ router.get('/register', userController.getRegister)
 router.post('/register', userController.postRegister)
 router.get('/login', userController.getLogin)
 router.post('/login', userController.postLogin)
+router.get('/logout', auth, userController.logout)
 
 export default router
