df019f8ecda6515e14b59cb90c7ecf8b0efba6f9
AlwanY <ebisukuru@gmail.com>
2020-12-11 13:35:03 +0700

    succes add method delete user, user bio, room

--- src/controller/dashboard.js
+++ src/controller/dashboard.js
@@ -32,6 +32,60 @@ class dashboardController {
     }
   }
 
+  static deleteUser = async (req, res) => {
+    try {
+      const { id } = req.params
+
+      const findHistory = await User.findOne({ where: { id } })
+
+      if (!findHistory) {
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      const deleted = await User.destroy({ where: { id } })
+
+      res.status(200).json({ message: 'Deleted User' })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  }
+
+  static deleteBio = async (req, res) => {
+    try {
+      const { id } = req.params
+
+      const findBio = await UserBio.findOne({ where: { id } })
+
+      if (!findBio) {
+        return res.status(404).json({ message: 'UserBio not found' })
+      }
+
+      const deleted = await UserBio.destroy({ where: { id } })
+
+      res.status(200).json({ message: 'Deleted UserBio' })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  }
+
+  static deleteHistory = async (req, res) => {
+    try {
+      const { id } = req.params
+
+      const findRoom = await Room.findOne({ where: { id } })
+
+      if (!findRoom) {
+        return res.status(404).json({ message: 'Room not found' })
+      }
+
+      const deleted = await Room.destroy({ where: { id } })
+
+      res.status(200).json({ message: 'Deleted Room' })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  }
+
 }
 
 export default dashboardController
--- src/router/dashboard.js
+++ src/router/dashboard.js
@@ -6,5 +6,11 @@ import dashboardController from '../controller/dashboard'
 const router = express.Router()
 
 router.get(PATH.DASHBOARD, dashboardController.getDashboard)
+router.get('/user', dashboardController.getAllUser)
+router.get('/user-bio', dashboardController.getAllBio)
+router.get('/user-history', dashboardController.getAllRoom)
+router.delete('/delete-user/:id', dashboardController.deleteUser)
+router.delete('/delete-bio/:id', dashboardController.deleteBio)
+router.delete('/delete-room/:id', dashboardController.deleteHistory)
 
 export default router
